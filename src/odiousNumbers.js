75590d4afb5e7265b1edc0c915fde4841e963abd
Jean Benitez <jkbdiaz@gmail.com>
2018-10-19 20:56:20 -0500

    odious number solution

--- src/odiousNumbers.js
+++ src/odiousNumbers.js
@@ -1,3 +1,8 @@
-export const odiousNumbers = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const odiousNumbers = (start, end) => {
+  const odious = [];
+  for (let number = start; number <= end; number++) {
+    const isOdiousNumber = number.toString(2).replace(/0/g, '').length % 2 !== 0;
+    isOdiousNumber && odious.push(number);
+  }
+  return odious;
 };
